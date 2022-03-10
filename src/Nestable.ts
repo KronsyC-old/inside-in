30bfc779afbe3350367417c704c25b5d294c4400
KronsyC <87078913+KronsyC@users.noreply.github.com>
2022-03-10 19:07:29 +0000

    Initial Commit

---
+++ src/Nestable.ts
@@ -0,0 +1,66 @@
+
+const kIsRoot = Symbol("Is Root?")
+const kChildren = Symbol("Children")
+const kParent = Symbol("Parent")
+export const kInheritedRawPropertyStore = Symbol("Inherited Raw Property Store")
+
+type StringIndexed = {
+    [x:string]:any
+}
+
+export default class Nestable{
+    private [kIsRoot]:boolean;
+    private [kChildren]:Nestable[] = [];
+    private [kParent]? : Nestable;
+    private [kInheritedRawPropertyStore]:StringIndexed = {}
+
+    constructor(parent?:Nestable){
+        this[kParent] = parent 
+        this[kIsRoot] = parent?false:true
+    }
+
+    public addChild(nestable:Nestable){
+        if(nestable === this){
+            throw new Error("Nestable cannot be it's own child")
+        }
+        if(!nestable[kParent]){
+            console.log("Orphan");
+            
+            nestable[kParent] = this
+            nestable[kIsRoot] = false
+        }
+        this[kChildren].push(nestable)
+    }
+    public getChildren(){
+        return this[kChildren]
+    }
+    public deepGetChildren(){
+        const children:Nestable[] = []
+        this[kChildren].forEach( child => {
+            children.push(child)
+            children.push(...child.deepGetChildren())
+        } )
+        return children
+    }
+    public getRootNode() : Nestable{
+        
+        if(this[kIsRoot]){
+            return this
+        }
+        else if(this[kParent]){
+            return this[kParent]!.getRootNode()
+        }
+        else{
+            throw new Error("Cannot Get Root Node For Nestable")
+        }
+    }
+    public getParent(){
+        if(this[kParent]){
+            return this[kParent]
+        }else{
+            return undefined
+        }
+    }
+
+}
+
---
+++ src/decorators/inherited.ts
@@ -0,0 +1,56 @@
+import Nestable, { kInheritedRawPropertyStore } from "../Nestable";
+import clone from "clone"
+
+type BoundToNestable = {
+    (this:Nestable):any
+}
+type BoundToNestableSetter = {
+    (this:Nestable, data:object):any
+}
+
+/**
+ * 
+ * @param overrideParentProperties Only Applies to Objects - Should conflicting keys from the parent be overriden
+ * @param deepCopy Should nested properties be deep-copied or just shallow copied (more performant)
+ * @returns 
+ */
+export default function Inherited(overrideParentProperties:boolean=true, deepCopy:boolean=false){
+
+    return function(target: Nestable, propertyKey: string){
+        // If it is an array or JSON, include the properties of the parent, and override depending on preference
+        // Otherwise, throw an error
+        const getter:BoundToNestable = function(){
+            const value = this[kInheritedRawPropertyStore][propertyKey]
+            const parent = this.getParent()
+            if(!parent){
+                return value
+            }
+            else{
+                //@ts-expect-error
+                let parentProps = parent[propertyKey]
+                // Form an aggregate of the parent and the current node
+                if(deepCopy){
+                     parentProps = clone(parentProps)
+                }
+
+                if(overrideParentProperties)return Object.assign(parentProps, value)
+                else return Object.assign(value, parentProps)
+            }
+        }
+        const setter:BoundToNestableSetter = function(data:object){
+            if(typeof data !== "object"){
+                throw new Error("@Inherited() Decorated Properties must be objects")
+            }
+            
+            this[kInheritedRawPropertyStore][propertyKey] = data
+            
+        }
+        Object.defineProperty(target, propertyKey, {
+            get: getter,
+            set: setter
+        })
+        
+        
+    }
+
+}
